e44d100444bc0fb16afca761b37a0eb0250d27d1
Aditya Sharma <aditya.sharma@zenatix.com>
2022-11-26 21:32:52 +0530

    Initial Comit
    Things learned
    	1. How to Create an object.
    	2. How library is imported.

---
+++ index.js
@@ -0,0 +1,14 @@
+// Including a library in the script
+var http = require('http');
+const func = require("./self_made_libary");
+
+console.log(func)
+
+// Creating a server which listenes to port 8080
+// http.createServer(function (req, res)
+// {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Hello World!');
+// }).listen(8080)
+
+//
---
+++ self_made_libary.js
@@ -0,0 +1,10 @@
+// Creating an object
+
+Obj = {
+    name: "Aditya",
+    Age: 3,
+    sex: "Male"
+}
+
+
+module.exports = Obj;
